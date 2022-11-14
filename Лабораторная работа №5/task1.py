6f27e421cca66d8af5bfae6705f16dcb22a982f0
Egowkins <115096011+Egowkins@users.noreply.github.com>
2022-11-14 16:01:29 +0300

    Update task1.py

--- Лабораторная работа №5/task1.py
+++ Лабораторная работа №5/task1.py
@@ -2,9 +2,6 @@ from pprint import pprint
 # TODO решить с помощью list comprehension и распечатать его
 num_of_dig = 16
 bit_list = []
-for i in range(num_of_dig):
-
-    bit_dic = {'bin': bin(i), 'dec' : i, 'hex': hex(i), 'oct': oct(i)}
-    bit_list += [bit_dic for _ in range(1)]
-
+bit_list = [{'bin': bin(i), 'dec' : i, 'hex': hex(i), 'oct': oct(i)} for i in range(num_of_dig)]
 pprint(bit_list)
+
